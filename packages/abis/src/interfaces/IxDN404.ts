0e9db990a4984f3206f500f5f5e39bb6cbb39ddb
Suho Lee <iam@suho.io>
2025-10-23 19:13:51 +0900

    Update TypeScript configuration and ABIs for xMorse and xMorseStaking
    - Enhanced TypeScript configuration in `packages/abis/tsconfig.json` for better compatibility and output management.
    - Added `.gitignore` entries for deployment files and TypeScript build artifacts.
    - Introduced new ABI files for `xMorseStaking` and updated existing ABIs for `xMorse`, `xMorseCollateral`, and `xDN404`.
    - Updated `index.ts` to include the new `IxMorseStaking` ABI.
    - Improved organization of ignored files in `.gitignore` for cleaner project structure.

--- packages/abis/src/interfaces/IxDN404.ts
+++ packages/abis/src/interfaces/IxDN404.ts
@@ -1,142 +1,142 @@
 const abi = [
-  {
-    "type": "function",
-    "name": "quoteTransferRemoteNFT",
-    "inputs": [
-      {
-        "name": "destination",
-        "type": "uint32",
-        "internalType": "uint32"
-      },
-      {
-        "name": "recipient",
-        "type": "bytes32",
-        "internalType": "bytes32"
-      },
-      {
-        "name": "tokenIds",
-        "type": "uint256[]",
-        "internalType": "uint256[]"
-      }
-    ],
-    "outputs": [
-      {
-        "name": "",
-        "type": "tuple[]",
-        "internalType": "struct Quote[]",
-        "components": [
-          {
-            "name": "token",
-            "type": "address",
-            "internalType": "address"
-          },
-          {
-            "name": "amount",
-            "type": "uint256",
-            "internalType": "uint256"
-          }
-        ]
-      }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "quoteTransferRemoteNFTPartial",
-    "inputs": [
-      {
-        "name": "destination",
-        "type": "uint32",
-        "internalType": "uint32"
-      },
-      {
-        "name": "tokenId",
-        "type": "uint256",
-        "internalType": "uint256"
-      },
-      {
-        "name": "recipients",
-        "type": "bytes32[]",
-        "internalType": "bytes32[]"
-      },
-      {
-        "name": "amounts",
-        "type": "uint256[]",
-        "internalType": "uint256[]"
-      }
-    ],
-    "outputs": [
-      {
-        "name": "",
-        "type": "tuple[]",
-        "internalType": "struct Quote[]",
-        "components": [
-          {
-            "name": "token",
-            "type": "address",
-            "internalType": "address"
-          },
-          {
-            "name": "amount",
-            "type": "uint256",
-            "internalType": "uint256"
-          }
-        ]
-      }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "function",
-    "name": "transferRemoteNFT",
-    "inputs": [
-      {
-        "name": "destination",
-        "type": "uint32",
-        "internalType": "uint32"
-      },
-      {
-        "name": "recipient",
-        "type": "bytes32",
-        "internalType": "bytes32"
-      },
-      {
-        "name": "tokenIds",
-        "type": "uint256[]",
-        "internalType": "uint256[]"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "payable"
-  },
-  {
-    "type": "function",
-    "name": "transferRemoteNFTPartial",
-    "inputs": [
-      {
-        "name": "destination",
-        "type": "uint32",
-        "internalType": "uint32"
-      },
-      {
-        "name": "tokenId",
-        "type": "uint256",
-        "internalType": "uint256"
-      },
-      {
-        "name": "recipients",
-        "type": "bytes32[]",
-        "internalType": "bytes32[]"
-      },
-      {
-        "name": "amounts",
-        "type": "uint256[]",
-        "internalType": "uint256[]"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "payable"
-  }
-] as const;
+    {
+      "type": "function",
+      "name": "quoteTransferRemoteNFT",
+      "inputs": [
+        {
+          "name": "destination",
+          "type": "uint32",
+          "internalType": "uint32"
+        },
+        {
+          "name": "recipient",
+          "type": "bytes32",
+          "internalType": "bytes32"
+        },
+        {
+          "name": "tokenIds",
+          "type": "uint256[]",
+          "internalType": "uint256[]"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "",
+          "type": "tuple[]",
+          "internalType": "struct Quote[]",
+          "components": [
+            {
+              "name": "token",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "amount",
+              "type": "uint256",
+              "internalType": "uint256"
+            }
+          ]
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "quoteTransferRemoteNFTPartial",
+      "inputs": [
+        {
+          "name": "destination",
+          "type": "uint32",
+          "internalType": "uint32"
+        },
+        {
+          "name": "tokenId",
+          "type": "uint256",
+          "internalType": "uint256"
+        },
+        {
+          "name": "recipients",
+          "type": "bytes32[]",
+          "internalType": "bytes32[]"
+        },
+        {
+          "name": "amounts",
+          "type": "uint256[]",
+          "internalType": "uint256[]"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "",
+          "type": "tuple[]",
+          "internalType": "struct Quote[]",
+          "components": [
+            {
+              "name": "token",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "amount",
+              "type": "uint256",
+              "internalType": "uint256"
+            }
+          ]
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "transferRemoteNFT",
+      "inputs": [
+        {
+          "name": "destination",
+          "type": "uint32",
+          "internalType": "uint32"
+        },
+        {
+          "name": "recipient",
+          "type": "bytes32",
+          "internalType": "bytes32"
+        },
+        {
+          "name": "tokenIds",
+          "type": "uint256[]",
+          "internalType": "uint256[]"
+        }
+      ],
+      "outputs": [],
+      "stateMutability": "payable"
+    },
+    {
+      "type": "function",
+      "name": "transferRemoteNFTPartial",
+      "inputs": [
+        {
+          "name": "destination",
+          "type": "uint32",
+          "internalType": "uint32"
+        },
+        {
+          "name": "tokenId",
+          "type": "uint256",
+          "internalType": "uint256"
+        },
+        {
+          "name": "recipients",
+          "type": "bytes32[]",
+          "internalType": "bytes32[]"
+        },
+        {
+          "name": "amounts",
+          "type": "uint256[]",
+          "internalType": "uint256[]"
+        }
+      ],
+      "outputs": [],
+      "stateMutability": "payable"
+    }
+  ] as const;
 
 export default abi;
--- packages/abis/src/interfaces/index.ts
+++ packages/abis/src/interfaces/index.ts
@@ -1,11 +1,13 @@
 import IDN404Abi from './IDN404.js';
 import IMorseAbi from './IMorse.js';
 import IxDN404Abi from './IxDN404.js';
+import IxMorseStakingAbi from './IxMorseStaking.js';
 
 const abis = {
   IDN404: IDN404Abi,
   IMorse: IMorseAbi,
   IxDN404: IxDN404Abi,
+  IxMorseStaking: IxMorseStakingAbi,
 };
 
 export default abis;
